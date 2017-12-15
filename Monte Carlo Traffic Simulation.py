5c915b833643dc759646b722c20ac84f87f8a2ae
xz28 <xz28@illinois.edu>
2017-12-14 18:21:47 -0600

    add some contents.

--- Monte Carlo Traffic Simulation.py
+++ Monte Carlo Traffic Simulation.py
@@ -8,46 +8,72 @@ import queue
 import time
 
 
-# In[2]:
 
 class car:
+    '''
+    >>> carA = car('Tesla')
+    >>> carA.name
+    Tesla
+    >>> car.carA.length
+    2.5
+    >>> car.carA.speed
+    11.176
+    '''
+
     def __init__(self, name='noNameCar'):
         self.name = name
         self.length = 2.5
-        self.speed = 11.176
-        self.acceleration = 0
+        self.speed = np.random.beta(6, 2, 1)+11.176
 
 
-# In[3]:
 
 class traffic_light:
+    """
+        >>> tlA=traffic_light()
+        >>> print(tlA.turnTime)
+        30
+        >>> print(tlA.Colors)
+        ['G', 'R']
+        """
     def __init__(self):
         self.turnTime = 30
         self.Colors = ['G', 'R']
 
 
-# In[4]:
 
 class road:
     def __init__(self, length=3000):
         self.length = length
         self.cars = pd.DataFrame(
-            index=['Length', 'Speed', 'Acceleration', 'CurrentLoc', 'RemainDis', 'NextLight', 'MoveTime', 'StopTime'])
+            index=['Length', 'Speed', 'CurrentLoc', 'RemainDis', 'NextLight', 'MoveTime', 'StopTime'])
         self.trafficLights = pd.DataFrame(index=['TurnTime', 'Colors', 'CurrentColor', 'Location'])
 
     def put_car(self, numCars=0):
+        '''
+        :param numCars: The number of the cars the user
+        :return: A dataframe contains all the index about the cars. For example, if we put one car, it will
+                has the car's  'Length', 'Speed', 'CurrentLoc', 'RemainDis', 'NextLight', 'MoveTime', 'StopTime'.
+                The initial value for all car will be same.
+        >>> road.put_car(numCars=1)
+
+        '''
         for i in range(numCars):
             temp = {}
             tempCar = car(i)
             temp['{}'.format(i)] = {'Length': tempCar.length, 'Speed': tempCar.speed,
-                                    'Acceleration': tempCar.acceleration, 'CurrentLoc': 0, 'RemainDis': 1500,
+                                     'CurrentLoc': 0, 'RemainDis': 1500,
                                     'NextLight': None, 'MoveTime': 0, 'StopTime': 0}
             temp = pd.DataFrame(temp)
             self.cars = self.cars.join(temp)
         self.number_of_cars = len(self.cars.columns)
-        print(self.cars)
 
     def put_trafficLight(self, location=1000, lightName='noNameLight', turnTime=30):
+        '''
+        :param location: The location for the trifficlight and initial value is 1000m.
+        :param lightName: The name of the trifficlight.
+        :param turnTime: The turning time of the trifficlight.
+        :return:  A dataframe contains all the index about the trifficlight.
+        '''
         temp = {}
         tempLig = traffic_light()
         temp[lightName] = {'TurnTime': turnTime, 'Colors': tempLig.Colors, 'CurrentColor': 'G', 'Location': location}
@@ -56,15 +82,12 @@ class road:
         print(self.trafficLights)
 
 
-# In[5]:
 
 Green_road = road()
 
-# In[7]:
 
 Green_road.put_car(int(input('Please input how many cars you want to simulate:')))
 
-# In[8]:
 
 Green_road.put_trafficLight(lightName='0', turnTime=120, location=400)
 Green_road.put_trafficLight(lightName='1', turnTime=120, location=700)
@@ -73,9 +96,13 @@ Green_road.put_trafficLight(lightName='2', turnTime=120, location=1400)
 Green_road.put_trafficLight(lightName='7', turnTime=9999999, location=99999999)
 
 
-# In[13]:
 
 def which_light_next(car, lights):
+    '''
+    :param car: Each car.
+    :param lights: The dataframe of all triffic light together.
+    :return: The name of the next light.
+    '''
     count = 0
 
     for i in range(len(lights.columns)):
@@ -97,17 +124,26 @@ def which_light_next(car, lights):
         return next_light
 
 
-# In[15]:
 
 def change_light_color(lights, time):
+    '''
+    :param lights: The dataframe of all triffic light together.
+    :param time: The time begins when the first car came in and by now.
+    :return:
+    '''
     for i in range(len(lights.columns)):
         if (time / 10) % lights.iloc[:, i].TurnTime == 0:  # time change
             lights.iloc[:, i].CurrentColor = list(set(lights.iloc[:, i].Colors) - set(lights.iloc[:, i].CurrentColor))
 
 
-# In[18]:
 
 def car_collison(cars, car_Num):
+    '''
+    :param cars: The dataframe of all cars together.
+    :param car_Num: The number of the cars.
+    :return: If the car can keep going for another second or not.
+    >>>
+    '''
     if car_Num > 0:
         if (cars.iloc[:, car_Num - 1].CurrentLoc - cars.iloc[:, car_Num].CurrentLoc) >= 1.5:  # time change
             return False
